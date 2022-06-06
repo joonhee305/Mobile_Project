f2b62d9c5b072794b5fd680a4ecef9793e982489
Song <misosjm@naver.com>
2022-06-06 17:25:06 +0900

    프래그먼트에서 액티비티로 화면전환

--- app/src/main/java/com/cookandroid/mobile_project/MainFragment.java
+++ app/src/main/java/com/cookandroid/mobile_project/MainFragment.java
@@ -1,20 +1,35 @@
 package com.cookandroid.mobile_project;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class MainFragment extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View v =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button btn_addList = (Button) v.findViewById(R.id.addList);
+
+        btn_addList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), AddListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
     }
 }
