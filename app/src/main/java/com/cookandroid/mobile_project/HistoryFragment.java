b9ba5926021e42e88e15db3d26cf7ddeed17c211
Song <misosjm@naver.com>
2022-06-18 14:32:32 +0900

    지난일정 기능 완성

--- app/src/main/java/com/cookandroid/mobile_project/HistoryFragment.java
+++ app/src/main/java/com/cookandroid/mobile_project/HistoryFragment.java
@@ -1,16 +1,20 @@
 package com.cookandroid.mobile_project;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +26,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Calendar;
 
 public class HistoryFragment extends Fragment {
@@ -30,8 +36,6 @@ public class HistoryFragment extends Fragment {
     SQLiteOpenHelper myHelper;
     LinearLayout linearHistory;
     CalendarView calendarView;
-    Button btnImgTest;
-    TextView todo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,15 +43,23 @@ public class HistoryFragment extends Fragment {
         View v =  inflater.inflate(R.layout.fragment_history, container, false);
         myHelper=new myDBHelper(getActivity());
         calendarView = (CalendarView) v.findViewById(R.id.calendarView);
-        todo = (TextView) v.findViewById(R.id.tvTodo);
         sqLiteDatabase=myHelper.getWritableDatabase();
         linearHistory = v.findViewById(R.id.linearHistory);
-        btnImgTest = v.findViewById(R.id.btnImgTest);
+
+        long cur = calendarView.getDate();
+
+        calendarView.setDate(cur-86400*1000L);
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
-                Cursor historyCursor=sqLiteDatabase.rawQuery("select * from historyTBL where hDate = ('"+year+""+month+""+day+"')",null);
+                String sMonth = Integer.toString(month+1);
+                String sDay = Integer.toString(day);
+                if(month<10) sMonth = "0"+Integer.toString(month+1);
+                if(day<10) sDay = "0"+Integer.toString(day);
+                linearHistory.removeAllViews();
+                String date = year+sMonth+sDay;
+                Cursor historyCursor=sqLiteDatabase.rawQuery("select * from historyTBL where hDate = ('"+date+"')", null);
                 while (historyCursor.moveToNext()) {
                     Button btn=new Button(getActivity());
                     ImageView myImage = new ImageView(getActivity());
@@ -56,40 +68,47 @@ public class HistoryFragment extends Fragment {
                     int hTime=historyCursor.getInt(3);
                     String hPath=historyCursor.getString(4);
 
-//                    String value=tName+" "+historyCursor(tTime);
-//                    btn.setText(value);
-//                    btn.setId(tId);
                     btn.setText(hName+" "+hTime);
                     btn.setOnClickListener(new View.OnClickListener() {
-                        Integer btnId=btn.getId();
                         @Override
                         public void onClick(View v) {
-                            File file = new File(hPath);
-                            if(file.exists()){
-                                Bitmap myBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-                                myImage.setImageBitmap(myBitmap);
+                            File storageDir = new File(getActivity().getFilesDir() + "/capture");
+                            String filename = hPath + ".jpg";
+                            File file = new File(storageDir, filename);
+                            try {
+                                Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
 
+                                Dialog myPicture;
+                                myPicture=new Dialog(getActivity());
+                                myPicture.setContentView(R.layout.dialog_mypicture);
 
+                                //다이얼 로그 내 버튼
+                                TextView dlTitle=myPicture.findViewById(R.id.dlMyPictureTitle);
+                                ImageView dlPicture=myPicture.findViewById(R.id.dlMyPictureImage);
+                                Button dlFinish=myPicture.findViewById(R.id.dlMyPictureFinish);
+                                dlFinish.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        myPicture.dismiss();
+                                    }
+                                });
+                                dlPicture.setImageBitmap(bitmap);
+                                dlTitle.setText(hName);
+                                myPicture.show();
+
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
                             }
+
                         }
 
                     });
                     linearHistory.addView(btn);
-                    linearHistory.addView(myImage);
                 }
 
             }
 
         });
-        btnImgTest.setOnClickListener(new View.OnClickListener() {
-            int a = 0;
-            @Override
-            public void onClick(View view) {
-//                sqLiteDatabase.execSQL("insert into historyTBL values('일정','test','20220617',a,' ');");
-//                a++;
-            }
-        });
-
 
         return v;
 
